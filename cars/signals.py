f11f306d46dac6bf8f46a6aa7eed8204509917ab
joaomacedocb <joaomacedocb@gmail.com>
2024-09-27 06:21:46 -0300

    Ajustado nome da funcao item_inventory_update para snake case

--- cars/signals.py
+++ cars/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 
 from cars.models import Car, ItemInventory
 
-def itemInventoryUpdate():
+def item_inventory_update():
     items_count = Car.objects.all().count()
     items_value = Car.objects.all().aggregate(
         total_value = Sum('value')
@@ -17,8 +17,8 @@ def itemInventoryUpdate():
 
 @receiver(post_save, sender=Car)
 def item_post_save(sender, instance, **kwargs):
-    itemInventoryUpdate()
+    item_inventory_update()
 
 @receiver(post_delete, sender=Car)
 def item_post_delete(sender, instance, **kwargs):
-    itemInventoryUpdate()
+    item_inventory_update()
